0ece25334f1b7e748fe5c4af91c13894e1756c1e
derekTurner <derek.turner@uws.ac.uk>
2025-03-04 10:39:05 +0000

    walkwithdirection
    player strides ijkl

--- src/scripts/farmer.ts
+++ src/scripts/farmer.ts
@@ -1,4 +1,4 @@
-import { AnimationGroup, Vector3 } from "@babylonjs/core";
+import { AnimationGroup, Quaternion, Vector3 } from "@babylonjs/core";
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
 
 import { IScript } from "babylonjs-editor-tools";
@@ -6,7 +6,10 @@ import { IScript } from "babylonjs-editor-tools";
 export default class SceneComponent implements IScript {
     
     private ratio: number; 
-    private step:Vector3 = new Vector3(0,0,7);
+    private stepf:Vector3 = new Vector3(0,0,7); // +z
+    private stepb:Vector3 = new Vector3(0,0,-7);// -z
+    private stepr:Vector3 = new Vector3(7,0,0); // +x
+    private stepl:Vector3 = new Vector3(-7,0,0);// -x
 
     private deathAnim:          AnimationGroup | null;
     private gun_ShootAnim:      AnimationGroup | null;
@@ -34,12 +37,19 @@ export default class SceneComponent implements IScript {
     private waveAnim:           AnimationGroup | null;
 
     private animating: boolean = false;
-    private keyDownMap: { [key: string]: boolean } | null;
+    private keyDownMap: { [key: string]: boolean } | null = null;
+
+    private forward: Quaternion = Quaternion.RotationAxis(new Vector3(0, 1, 0), (2 * Math.PI / 2));
+    private backward: Quaternion = Quaternion.RotationAxis(new Vector3(0, 1, 0), (0 * Math.PI / 2));
+    private left: Quaternion = Quaternion.RotationAxis(new Vector3(0, 1, 0), (1 * Math.PI / 2));
+    private right: Quaternion = Quaternion.RotationAxis(new Vector3(0, 1, 0), (3 * Math.PI / 2));
 
         public constructor(public mesh: Mesh) {
             this.ratio = this.mesh.getScene().getAnimationRatio();   
-            this.step = this.step.scale(this.ratio);  
-            
+            this.stepr = this.stepr.scale(this.ratio);  
+            this.stepl = this.stepl.scale(this.ratio);
+            this.stepf = this.stepf.scale(this.ratio);
+            this.stepb = this.stepb.scale(this.ratio);
             
 
             //model animation groups
@@ -54,8 +64,8 @@ export default class SceneComponent implements IScript {
             this.idle_NeutralAnim = this.mesh.getScene().getAnimationGroupByName("Idle_Neutral");//G
             this.idle_SwordAnim = this.mesh.getScene().getAnimationGroupByName("Idle_Sword");//H
             this.interactAnim = this.mesh.getScene().getAnimationGroupByName("Interact");//I
-            this.kick_LeftAnim = this.mesh.getScene().getAnimationGroupByName("Kick_Left");//k
-            this.kick_RightAnim = this.mesh.getScene().getAnimationGroupByName("Kick_Right");//K
+            this.kick_LeftAnim = this.mesh.getScene().getAnimationGroupByName("Kick_Left");//v
+            this.kick_RightAnim = this.mesh.getScene().getAnimationGroupByName("Kick_Right");//V
             this.punch_LeftAnim = this.mesh.getScene().getAnimationGroupByName("Punch_Left");//p
             this.punch_RightAnim = this.mesh.getScene().getAnimationGroupByName("Punch_Right");//P
             this.rollAnim = this.mesh.getScene().getAnimationGroupByName("Roll");//J
@@ -78,14 +88,69 @@ export default class SceneComponent implements IScript {
 
         this.keyDownMap = this.mesh.getScene().getExternalData("keyDownMap");
         console.log(this.keyDownMap);
-        if (this.keyDownMap && this.keyDownMap["y"]) {
+        // using jkli to move as wasd is used for camera movement
+        // GLTF use quaternions for rotation so not mesh.rotation.y = value;
+        //https://doc.babylonjs.com/features/featuresDeepDive/mesh/transforms/center_origin/rotation_quaternions
+        var keydown = false;
+        if (this.keyDownMap!["j"] ) {
             this.idleAnim!.stop();
             this.walkAnim!.start(true);
-            this.mesh.position.addInPlace(this.step);
-            ////this.mesh.position.z += 0.1;
-            //this.mesh.rotation.y = 0* Math.PI / 2;
-            //keydown = true;
-            console.log("y key is down");
+            this.mesh.position.addInPlace(this.stepl);
+            //this.mesh.rotation.y = 3 * Math.PI / 2;
+            this.mesh.rotationQuaternion = this.left;
+            keydown = true;
         }
+        if (this.keyDownMap!["k"] ) {
+            this.idleAnim!.stop();
+            this.walkAnim!.start(true);
+            this.mesh.position.addInPlace(this.stepb);
+            //this.mesh.rotation.y = 2 * Math.PI / 2;
+            this.mesh.rotationQuaternion = this.backward;
+            keydown = true;
+        }
+        if (this.keyDownMap!["l"] ) {
+            this.idleAnim!.stop();
+            this.walkAnim!.start(true);
+            this.mesh.position.addInPlace(this.stepr);
+            //this.mesh.rotation.y = 1 * Math.PI / 2;
+            this.mesh.rotationQuaternion = this.right;
+            keydown = true;
+        }
+        if (this.keyDownMap!["i"] ) {
+            this.idleAnim!.stop();
+            this.walkAnim!.start(true);
+            this.mesh.position.addInPlace(this.stepf);
+            //this.mesh.rotation.y = 0* Math.PI / 2;
+            this.mesh.rotationQuaternion = this.forward;
+            keydown = true;
+        }
+    
+        if(!keydown){
+            //animating = false;
+            this.walkAnim!.stop();
+            this.idleAnim!.start(true);
+        }
+    
+        //trigger non- looping animations from keyboard for testing
+        //https://doc.babylonjs.com/typedoc/classes/BABYLON.AnimationGroup#start
+        if (this.keyDownMap!["x"] || this.keyDownMap!["X"]) {
+            //stopMe();
+            this.deathAnim!.start(false, 1.0, this.deathAnim!.from, this.deathAnim!.to, false);
+        }
+        
+        if (this.keyDownMap!["t"] || this.keyDownMap!["T"]) {
+            this.gun_ShootAnim!.start(true);
+        }
+        if (this.keyDownMap!["y"] || this.keyDownMap!["Y"]) {
+            this.hitRecieveAnim!.start(true);
+        }
+        if (this.keyDownMap!["o"] || this.keyDownMap!["O"]) {
+            this.idle_GunAnim!.start(true);
+        }
+        if (this.keyDownMap!["r"] || this.keyDownMap!["R"]) {
+            this.idle_Gun_PointingAnim!.start(true);
+        }
+
+
     }
 }
