42cf370f591327663dca6da75dd61e42c1f6e3a6
derekTurner <derek.turner@uws.ac.uk>
2025-04-22 08:53:07 +0100

    collision
    animated platform 1 cycle on collide

--- src/scripts/collisions.ts
+++ src/scripts/collisions.ts
@@ -6,23 +6,37 @@ import {
   Vector3,
   TransformNode,
   Nullable,
+  Animation,
+  PhysicsPrestepType,
+  Animatable,
 } from "@babylonjs/core";
 import HavokPhysics, { HavokPhysicsWithBindings } from "@babylonjs/havok";
 import { IScript } from "babylonjs-editor-tools";
+
+interface PositionArray {
+  frame: number;
+  value: number;
+}
 
 export default class SceneComponent implements IScript {
   private ground: Nullable<TransformNode> = null;
   private platform1: Nullable<TransformNode> = null;
   private platform2: Nullable<TransformNode> = null;
   private platform3: Nullable<TransformNode> = null;
+  private cube1: Nullable<TransformNode> = null;
   private cube2: Nullable<TransformNode> = null;
   private player: Nullable<TransformNode> = null;
   private groundAggregate: Nullable<PhysicsAggregate> = null;
   private platform1Aggregate: Nullable<PhysicsAggregate> = null;
   private platform2Aggregate: Nullable<PhysicsAggregate> = null;
   private platform3Aggregate: Nullable<PhysicsAggregate> = null;
+  private cube1Aggregate: Nullable<PhysicsAggregate> = null;
   private cube2Aggregate: Nullable<PhysicsAggregate> = null;
   private playerAggregate: Nullable<PhysicsAggregate> = null;
+
+  //animation parameters
+  private  frameRate: number = 30;
+  private anim1: Animatable ;
 
 
   private async goHavok(scene: Scene): Promise<HavokPlugin> {
@@ -35,6 +49,55 @@ export default class SceneComponent implements IScript {
     scene.enablePhysics(new Vector3(0, -9.81, 0), hk);
     return hk;
   }
+
+  // animation which can be pushed to the animations array of a mesh
+  private animation1 = () => {
+    const range = 200;
+    const xSlide = new Animation(
+      "xSlide",
+      "position.x",
+      this.frameRate,
+      Animation.ANIMATIONTYPE_FLOAT,
+      Animation.ANIMATIONLOOPMODE_CYCLE
+    );
+    const keyFramesX: PositionArray[] = [];
+    keyFramesX.push({ frame: 0, value: range });
+    keyFramesX.push({ frame: this.frameRate, value: -1 * range });
+    keyFramesX.push({
+      frame: 2 * this.frameRate - 1,
+      value:
+        -1 * range +
+        (2 * range * (this.frameRate / 2 - 1)) / (this.frameRate / 2),
+    });
+    xSlide.setKeys(keyFramesX);
+    return xSlide;
+  };
+
+  // functions to call on collision
+  private collideCB =  (collision: {
+    // log collisions
+    collider: { transformNode: { name: any } };
+    collidedAgainst: { transformNode: { name: any } };
+    point: any;
+    distance: any;
+    impulse: any;
+    normal: any;
+  }) => {
+    console.log(
+      "collideCB",
+      collision.collider.transformNode.name,
+      collision.collidedAgainst.transformNode.name,
+      collision.point,
+      collision.distance,
+      collision.impulse,
+      collision.normal
+    );
+    this.anim1 = this.scene.beginAnimation(this.platform1, 0, 2 * this.frameRate, true);
+    //setTimeout(() => {this.anim1.stop()}, 5000);// loop for 5 seconds
+    this.anim1.onAnimationLoop = () => {
+      this.anim1.stop();
+    }; // stops on one cycle
+  };
 
   public constructor(public scene: Scene) {}
 
@@ -49,6 +112,8 @@ export default class SceneComponent implements IScript {
       this.platform2?.setEnabled(true);
       this.platform3 = this.scene.getMeshByName("Platform3");
       this.platform3?.setEnabled(true);
+      this.cube1 = this.scene.getMeshByName("Cube1");
+      this.cube1?.setEnabled(true);
       this.cube2 = this.scene.getMeshByName("Cube2");
       this.cube2?.setEnabled(true);
       this.player = this.scene.getMeshByName("CharacterDisplay");
@@ -64,18 +129,33 @@ export default class SceneComponent implements IScript {
       );
       this.groundAggregate.body.setCollisionCallbackEnabled(true);
       */
+
+      this.cube1Aggregate = new PhysicsAggregate(
+        this.cube1!,
+        PhysicsShapeType.BOX,
+        { mass: 0, restitution: 0.3, friction: 0.9 },
+        this.scene
+      );
+      this.cube1Aggregate.body.setCollisionCallbackEnabled(true);
+      this.cube1Aggregate.body.setPrestepType(PhysicsPrestepType.ACTION);
+      this.cube1Aggregate.transformNode.animations.push(this.animation1());
+      this.scene.beginAnimation(this.cube1, 0, 2 * this.frameRate, true);
+
       this.platform1Aggregate = new PhysicsAggregate(
         this.platform1!,
         PhysicsShapeType.BOX,
-        { mass: 0, restitution: 0.3, friction: 0.7 },
+        { mass: 0, restitution: 0.3, friction: 0.9 },
         this.scene
       );
       this.platform1Aggregate.body.setCollisionCallbackEnabled(true);
+      this.platform1Aggregate.body.setPrestepType(PhysicsPrestepType.ACTION);
+      this.platform1Aggregate.transformNode.animations.push(this.animation1());
+      //this.scene.beginAnimation(this.platform1, 0, 2 * this.frameRate, true);
 
       this.platform2Aggregate = new PhysicsAggregate(
         this.platform2!,
         PhysicsShapeType.BOX,
-        { mass: 0, restitution: 0.3, friction: 0.7 },
+        { mass: 0, restitution: 0.3, friction: 0.9 },
         this.scene
       );
       this.platform2Aggregate.body.setCollisionCallbackEnabled(true);
@@ -83,7 +163,7 @@ export default class SceneComponent implements IScript {
       this.platform3Aggregate = new PhysicsAggregate(
         this.platform3!,
         PhysicsShapeType.BOX,
-        { mass: 0, restitution: 0.3, friction: 0.7 },
+        { mass: 0, restitution: 0.3, friction: 0.9 },
         this.scene
       );
       this.platform3Aggregate.body.setCollisionCallbackEnabled(true);
@@ -91,7 +171,7 @@ export default class SceneComponent implements IScript {
       this.cube2Aggregate = new PhysicsAggregate(
         this.cube2!,
         PhysicsShapeType.BOX,
-        { mass: 0.5, restitution: 0.3, friction: 0.7 },
+        { mass: 0.5, restitution: 0.3, friction: 0.9 },
         this.scene
       );
       this.cube2Aggregate.body.setCollisionCallbackEnabled(true);
@@ -99,37 +179,20 @@ export default class SceneComponent implements IScript {
       this.playerAggregate = new PhysicsAggregate(
         this.player!,
         PhysicsShapeType.BOX,
-        { mass: 0, restitution: 0.3, friction: 0.7 },
+        { mass: 0, restitution: 0.3, friction: 0.9 },
         this.scene
       );
 
-      // functions to call on collision  
-      var collideCB = function (collision: {
-        // log collisions
-        collider: { transformNode: { name: any } };
-        point: any;
-        distance: any;
-        impulse: any;
-        normal: any;
-      }) {
-        console.log(
-          "collideCB",
-          collision.collider.transformNode.name,
-          collision.point,
-          collision.distance,
-          collision.impulse,
-          collision.normal
-        );
-      };
-      //this.groundAggregate.body.getCollisionObservable().add(collideCB);
-      this.platform1Aggregate.body.getCollisionObservable().add(collideCB);
-      this.platform2Aggregate.body.getCollisionObservable().add(collideCB);
-      this.platform3Aggregate.body.getCollisionObservable().add(collideCB);
-      //this.cube2Aggregate.body.getCollisionObservable().add(collideCB);
       
+      //this.groundAggregate.body.getCollisionObservable().add(this.collideCB);
+      this.platform1Aggregate.body.getCollisionObservable().add(this.collideCB);
+      //this.platform2Aggregate.body.getCollisionObservable().add(this.collideCB);
+      //this.platform3Aggregate.body.getCollisionObservable().add(this.collideCB);
+      //this.cube2Aggregate.body.getCollisionObservable().add(this.collideCB);
       
     });
   }
+
   public onUpdate(): void {
     //this.platform2?.dispose();
   }
